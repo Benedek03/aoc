06a4e2ccc0968739289568bcd2e905ee747f1223
Szanka Benedek <szankabg@gmail.com>
2024-09-09 18:58:32 +0200

    2021 10

---
+++ 2021/day10.cpp
@@ -0,0 +1,101 @@
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+#include "../common/puzzle.hpp"
+
+using namespace std;
+
+class Solution : public Puzzle {
+    vector<long> v;
+
+   public:
+    Solution(std::string inputFileName = "inputs/2021/10.txt")
+        : Puzzle(inputFileName) {}
+    std::string part1() {
+        int result1 = 0;
+        for (int i = 0; i < 106; i++) {
+            char c;
+
+            string line;
+            getline(inputFile, line);
+            stringstream ss(line);
+
+            stack<char> s;
+            while (ss >> c) {
+                if (c == '(')
+                    s.push(c);
+                else if (c == '[')
+                    s.push(c);
+                else if (c == '{')
+                    s.push(c);
+                else if (c == '<')
+                    s.push(c);
+
+                else if (c == ')') {
+                    if (s.top() == '(')
+                        s.pop();
+                    else {
+                        result1 += 3;
+                        break;
+                    }
+                } else if (c == ']') {
+                    if (s.top() == '[')
+                        s.pop();
+                    else {
+                        result1 += 57;
+                        break;
+                    }
+                } else if (c == '}') {
+                    if (s.top() == '{')
+                        s.pop();
+                    else {
+                        result1 += 1197;
+                        break;
+                    }
+                } else if (c == '>') {
+                    if (s.top() == '<')
+                        s.pop();
+                    else {
+                        result1 += 25137;
+                        break;
+                    }
+                }
+            }
+
+            if (!ss.eof())
+                while (ss >> c);
+            else if (s.size() > 0) {
+                long curr = 0;
+                while (!s.empty()) {
+                    curr *= 5;
+                    if (s.top() == '(') {
+                        s.pop();
+                        curr += 1;
+                    } else if (s.top() == '[') {
+                        s.pop();
+                        curr += 2;
+                    } else if (s.top() == '{') {
+                        s.pop();
+                        curr += 3;
+                    } else if (s.top() == '<') {
+                        s.pop();
+                        curr += 4;
+                    }
+                }
+                v.push_back(curr);
+            }
+        }
+        return to_string(result1);
+    }
+    std::string part2() {
+        sort(v.begin(), v.end());
+        return to_string(v[v.size() / 2]);
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    s.solve();
+}
