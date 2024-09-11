fca5c6672d244b95c98cf2586404a6b8910b8916
Szanka Benedek <szankabg@gmail.com>
2024-09-11 11:28:47 +0200

    2016 04

---
+++ year2016/day04.java
@@ -0,0 +1,116 @@
+package year2016;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import common.puzzle;
+
+public class day04 extends puzzle {
+    public day04() {
+        super("./inputs/2016/4.txt");
+    }
+
+    public static void main(String[] args) {
+        new day04().solve();
+    }
+
+    protected String part1() {
+        Scanner reader = getScanner();
+        int sum = 0;
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+
+            int index = 0;
+
+            Map<Character, Integer> m = new HashMap<Character, Integer>();
+
+            while (!Character.isDigit(line.charAt(index))) {
+                Character c = line.charAt(index);
+                if (c == '-') {
+                    index++;
+                    continue;
+                }
+
+                if (m.get(c) == null) {
+                    m.put(c, 1);
+                } else {
+                    m.merge(c, 1, Integer::sum);
+                }
+                index++;
+            }
+            Object[] list = m.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).map(e -> e.getKey())
+                    .collect(Collectors.toList()).toArray();
+            String validName = "";
+            for (int i = 0; i < 5; i++) {
+                validName += list[i];
+            }
+
+            String str[] = line.substring(index).split("\\[");
+            if (str[1].substring(0, 5).equals(validName))
+                sum += Integer.parseInt(str[0]);
+        }
+        return Integer.toString(sum);
+    }
+
+    private char shiftletter(int c, int s) {
+        c += s % 26;
+        if (c < 'a')
+            c += 26;
+        else if (c > 'z')
+            c -= 26;
+        return (char) c;
+    }
+
+    protected String part2() {
+        Scanner reader = getScanner();
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+
+            int index = 0;
+
+            Map<Character, Integer> m = new HashMap<Character, Integer>();
+
+            while (!Character.isDigit(line.charAt(index))) {
+                Character c = line.charAt(index);
+                if (c == '-') {
+                    index++;
+                    continue;
+                }
+
+                if (m.get(c) == null) {
+                    m.put(c, 1);
+                } else {
+                    m.merge(c, 1, Integer::sum);
+                }
+                index++;
+            }
+            Object[] list = m.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).map(e -> e.getKey())
+                    .collect(Collectors.toList()).toArray();
+            String validName = "";
+            for (int i = 0; i < 5; i++) {
+                validName += list[i];
+            }
+
+            String str[] = line.substring(index).split("\\[");
+            if (str[1].substring(0, 5).equals(validName)) {
+                String decrypted = "";
+                int shift = Integer.parseInt(str[0]);
+                for (char c : line.substring(0, index - 1).toCharArray()) {
+                    if (c == '-')
+                        decrypted += ' ';
+                    else
+                        decrypted += shiftletter(c, shift);
+                }
+                if (decrypted.equals("northpole object storage")) {
+                    return str[0];
+                }
+            }
+        }
+        return "TODO";
+    }
+}
